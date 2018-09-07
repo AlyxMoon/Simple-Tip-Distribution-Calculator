08fe381d82ffe3bf1ddb1f721a60674e854ef92c
Allister <allisterkmoon@gmail.com>
2018-09-07 08:35:02 -0700

    Break app into "pages"

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,67 @@
 <template>
   <div id="app">
-    <p>
-      Total Hours: {{ totalHours }}
-    </p>
-    <p>
-      Total Tips: {{ totalTips }}
-    </p>
-    <p>
-      Leftover Tips: {{ leftovers }}
-    </p>
-    <hr />
-    <p v-for="(bill, index) in bills" :key="`bill-${index}`">
-      <label for="bill">Number of {{ bill.type }}'s</label>
-      <input type="number" min="0" name="bill" v-model="bill.count" />
-    </p>
-    <p>
-      <label for="change">Amount of Change</label>
-      <input type="number" min="0" step="0.01" name="change" v-model.number="change" />
-    </p>
-    <hr />
-    <ul v-for="(data, type) in neededBills" :key="`neededBill-${type}`">
-      <li>
-        Number of {{ type }}'s -- {{ data.count }} -- diff: {{ data.diff}}
+    <ul class="page-navigation">
+      <li class="navigation-item" :class="{ active: page === 0 }">
+        <button @click="changePage(0)">1</button>
+      </li>
+      <li class="navigation-item" :class="{ active: page === 1 }">
+        <button @click="changePage(1)">2</button>
+      </li>
+      <li class="navigation-item" :class="{ active: page === 2 }">
+        <button @click="changePage(2)">3</button>
       </li>
     </ul>
-    <hr />
-    <template v-for="(employee, index) in employees" >
-      <p :key="`employee-info-${index}`">
-        <label for="name">Employee: </label>
-        <input type="text" name="name" v-model="employee.name" />
-        <label for="hours">Tippable Hours:</label>
-        <input type="number" min="0" name="hours" v-model.number="employee.hours" />
-        <span class="delete" @click.prevent="removeEmployee(index)">X</span>
+
+    <div v-if="page === 0">
+      <template v-for="(employee, index) in employees" >
+        <p :key="`employee-info-${index}`">
+          <label for="name">Employee: </label>
+          <input type="text" name="name" v-model="employee.name" />
+          <label for="hours">Tippable Hours:</label>
+          <input type="number" min="0" name="hours" v-model.number="employee.hours" />
+          <span class="delete" @click.prevent="removeEmployee(index)">X</span>
+        </p>
+        <p :key="`employee-tips-${index}`">
+          Tips Earned: {{ employeeTips(employee.hours) }}
+        </p>
+        <ul :key="`employee-bills-${index}`">
+          <template v-for="(bill, j) in employee.bills">
+            <li :key="`employee-${index}-bills-${j}`">
+              Number of {{ bill.type }}'s -- {{ bill.count }}
+            </li>
+          </template>
+        </ul>
+      </template>
+      <button @click.prevent="addEmployee">Add Employee</button>
+    </div>
+
+    <div v-if="page === 1">
+      <p v-for="(bill, index) in bills" :key="`bill-${index}`">
+        <label for="bill">Number of {{ bill.type }}'s</label>
+        <input type="number" min="0" name="bill" v-model="bill.count" />
       </p>
-      <p :key="`employee-tips-${index}`">
-        Tips Earned: {{ employeeTips(employee.hours) }}
+      <p>
+        <label for="change">Amount of Change</label>
+        <input type="number" min="0" step="0.01" name="change" v-model.number="change" />
       </p>
-      <ul :key="`employee-bills-${index}`">
-        <template v-for="(bill, j) in employee.bills">
-          <li :key="`employee-${index}-bills-${j}`">
-            Number of {{ bill.type }}'s -- {{ bill.count }}
-          </li>
-        </template>
+    </div>
+
+    <div v-if="page === 2">
+      <p>
+        Total Hours: {{ totalHours }}
+      </p>
+      <p>
+        Total Tips: {{ totalTips }}
+      </p>
+      <p>
+        Leftover Tips: {{ leftovers }}
+      </p>
+      <ul v-for="(data, type) in neededBills" :key="`neededBill-${type}`">
+        <li>
+          Number of {{ type }}'s -- {{ data.count }} -- diff: {{ data.diff}}
+        </li>
       </ul>
-    </template>
-    <button @click.prevent="addEmployee">Add Employee</button>
+    </div>
   </div>
 </template>
 
@@ -85,7 +102,8 @@ export default {
       },
       change: 0,
       totalTips: 0,
-      totalHours: 0
+      totalHours: 0,
+      page: 0
     }
   },
 
@@ -202,6 +220,10 @@ export default {
           }
         })
       })
+    },
+
+    changePage (newPage) {
+      this.page = newPage
     }
   },
 
@@ -222,6 +244,21 @@ export default {
   color: red;
   cursor: pointer;
   user-select: none;
+}
+
+.page-navigation {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.navigation-item {
+  display: inline-block;
+}
+
+.navigation-item.active button {
+  background-color: blue;
+  color: white;
 }
 
 </style>
